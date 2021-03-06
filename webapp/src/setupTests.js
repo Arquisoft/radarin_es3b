2cbe64f9ace788a51dd16cd1ca27647a0c87f290
Rcenador <rocio.cenador@solidlines.io>
2021-03-06 19:12:05 +0100

    added mockGeolacation setup

--- webapp/src/setupTests.js
+++ webapp/src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const mockGeolocation = {
+    getCurrentPosition: jest.fn()
+      .mockImplementationOnce((success) => Promise.resolve(success({
+        coords: {
+          latitude: 51.1,
+          longitude: 45.3
+        }
+      })))
+  };
+  global.navigator.geolocation = mockGeolocation;
