36e7ec6930b85e8e9566ac97e16670900438ee94
Rcenador <rocio.cenador@solidlines.io>
2021-03-06 19:37:15 +0100

    Update setupTests.js

--- webapp/src/setupTests.js
+++ webapp/src/setupTests.js
@@ -2,8 +2,8 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-import {jest} from '@jest/globals';
+import "@testing-library/jest-dom";
+import {jest} from "@jest/globals";
 
 const mockGeolocation = {
     getCurrentPosition: jest.fn()
