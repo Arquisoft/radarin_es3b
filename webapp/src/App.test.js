8f183a5921372faeb331ea8037492005da028da3
Rcenador <rocio.cenador@solidlines.io>
2021-03-06 18:56:31 +0100

    added test

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 test('renders learn react link', () => {
-  //render(<App />);
-  //const linkElement = screen.getByText(/Source code/i);
-  //expect(linkElement).toBeInTheDocument();
+  render(<App />);
+  const linkElement = screen.getByText(/Email address/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
