830af9763dc1aa9c5ebb6107e5ca4b4089a3ab04
UO246711 <UO246711@uniovi.es>
2021-03-08 19:27:04 +0100

    Test comentado

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import React from 'react';
-import ReactDOM from 'react-dom';
+//import { render, screen } from '@testing-library/react';
+//import App from './App';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
 
 test('renders learn react link', () => {
   //render(<App />);
