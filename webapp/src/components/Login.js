83a27fa8e3ce20ce22783d75659ef65614b5bcfe
Lu-UniOvi <lucigbu@gmail.com>
2021-03-12 16:43:35 +0100

    Create Login component
    De momento solo tiene el componente AuthButton

---
+++ webapp/src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Login extends React.Component {
+    
+    render() { 
+        return <AuthButton popup="https://solid.github.io/solid-auth-client/dist/popup.html"
+            login="log in for magic!" logout="log me outta here"/>
+        ;
+    }
+
+}
+
+export default Login;
