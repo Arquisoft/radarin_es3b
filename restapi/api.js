51397b05d23bb3637be2498c7727030ed867ee80
Rcenador <rocio.cenador@solidlines.io>
2021-03-30 11:21:35 +0200

    cleaned restapi/api.js

--- restapi/api.js
+++ restapi/api.js
@@ -1,102 +1,98 @@
-const express = require("express")
-const User = require("./models/users")
-const UserLocalization = require("./models/usersLocalization")
-const router = express.Router()
+const express = require("express");
+const User = require("./models/users");
+const UserLocalization = require("./models/usersLocalization");
+const router = express.Router();
 
 // Get all users
 router.get("/users/list", async (req, res) => {
-    const users = await User.find({}).sort('-_id') //Inverse order
-	res.send(users)
-})
+    const users = await User.find({}).sort('-_id'); //Inverse order
+    res.send(users);
+});
 
 //register a new user
 router.post("/users/add", async (req, res) => {
     let name = req.body.name;
     let email = req.body.email;
     //Check if the device is already in the db
-    let user = await User.findOne({ email: email })
-    if (user)
-        res.send({error:"Error: This user is already registered"})
-    else{
+    let user = await User.findOne({ email: email });
+    if (user){
+        res.send({ error: "Error: This user is already registered" });
+    } 
+    else {
         user = new User({
             name: name,
             email: email,
-        })
-        await user.save()
-        res.send(user)
+        });
+        await user.save();
+        res.send(user);
     }
-})
+});
 
 router.get("/userLocalization/delete/:user", async (req, res) => {
-    var user=req.params.user
+    var user = req.params.user;
 
-	
-	let userLocalization= await UserLocalization.findOne({ user: user })
-    if(userLocalization){
-		var response=await UserLocalization.findOneAndDelete({ user: user });
-		res.send(response)
+
+    let userLocalization = await UserLocalization.findOne({ user: user });
+    if (userLocalization) {
+        var response = await UserLocalization.findOneAndDelete({ user: user });
+        res.send(response);
     }
-	
-	else{
-		
-		res.send({error:"Error: El usuario no existe"})
-	}
+    else {
 
-	
+        res.send({ error: "Error: El usuario no existe" });
+    }
 
-})
+
+
+});
 
 router.get("/userLocalization/deleteAll", async (req, res) => {
-   
-	var response=await UserLocalization.deleteMany();
-	res.send(response)
-    
-	
-	
 
-	
+    var response = await UserLocalization.deleteMany();
+    res.send(response);
 
-})
+});
 
 router.get("/userLocalization/get/:user", async (req, res) => {
-    var user = req.params.user
-    let userLocalization= await UserLocalization.findOne({ user: user })
-    if(userLocalization){
-        res.send(userLocalization)
+    var user = req.params.user;
+    let userLocalization = await UserLocalization.findOne({ user: user });
+    if (userLocalization) {
+        res.send(userLocalization);
     }
 
-    else{
+    else {
         userLocalization = new UserLocalization({
             user: "error",
             latitude: 0,
             longitude: 0,
         })
-        res.send(userLocalization)
+        res.send(userLocalization);
     }
-})
+});
 
 
 
 router.post("/userLocalization/add", async (req, res) => {
     let user = req.body.user;
     let latitude = req.body.latitude;
-	let longitude = req.body.longitude;
-	
+    let longitude = req.body.longitude;
+
     //Check if the device is already in the db
-    let userLocalization= await UserLocalization.findOne({ user: user })
-    if (userLocalization)
-        UserLocalization.findOneAndUpdate({user: user}, req.body, function (err, response) {
-		res.send(response);
-});
-    else{
+    let userLocalization = await UserLocalization.findOne({ user: user });
+    if (userLocalization) {
+        UserLocalization.findOneAndUpdate({ user: user }, req.body, function (err, response) {
+            res.send(response);
+        });
+    }   
+    else {
         userLocalization = new UserLocalization({
             user: user,
             latitude: latitude,
-			longitude: longitude,
-        })
-        await userLocalization.save()
-        res.send(userLocalization)
+            longitude: longitude,
+        });
+        await userLocalization.save();
+        res.send(userLocalization);
     }
-})
+});
 
-module.exports = router
+module.exports = router;
