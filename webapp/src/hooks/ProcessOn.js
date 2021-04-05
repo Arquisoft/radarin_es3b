ae303d17adf3460287d08051a12d1c3089f9b387
Lu-UniOvi <lucigbu@gmail.com>
2021-04-05 10:43:23 +0200

    Añadidas notificaciones push de react-push-notification
    Se añadió una notificación de prueba en el processOn.js

--- webapp/src/hooks/ProcessOn.js
+++ webapp/src/hooks/ProcessOn.js
@@ -4,11 +4,17 @@ import WebId from "../components/solid/SolidId";
 import SolidFriends from "../components/solid/SolidFriends";
 import EncenderAplicacion from "../components/localizacion/EncenderAplicacion";
 
-
+import addNotification from 'react-push-notification'; //pushnotifs
 
 export default function processOn(){
 	
-	
+	addNotification({
+		title: 'Warning',
+		subtitle: 'This is a subtitle',
+		message: 'This is a very long message',
+		theme: 'darkblue',
+		native: true // when using native, your OS will handle theming.
+	}); //pushnotifs
 	
 	const webID=WebId();
 	const solidFriends=SolidFriends();
