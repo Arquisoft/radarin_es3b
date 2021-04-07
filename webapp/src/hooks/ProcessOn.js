692720f1d2258380114e506124216cdf97692863
Lu-UniOvi <lucigbu@gmail.com>
2021-04-07 14:09:22 +0200

    Base Notificaciones amigos

--- webapp/src/hooks/ProcessOn.js
+++ webapp/src/hooks/ProcessOn.js
@@ -5,23 +5,30 @@ import SolidFriends from "../components/solid/SolidFriends";
 import EncenderAplicacion from "../components/localizacion/EncenderAplicacion";
 
 import addNotification from 'react-push-notification'; //pushnotifs
+import friendNotification from "./FriendAppearedNotification";
+import FriendNotification from "../components/notifications/FriendNotifications";
+
 
 export default function processOn(){
 	
-	addNotification({
+	/*addNotification({
 		title: 'Warning',
 		subtitle: 'This is a subtitle',
 		message: 'This is a very long message',
 		theme: 'darkblue',
 		native: true // when using native, your OS will handle theming.
-	}); //pushnotifs
+	}); //pushnotifs*/
 	
 	const webID=WebId();
 	const solidFriends=SolidFriends();
+
+	//friendNotification(solidFriends);
 	 
 	return (
-	
+		<div>
 		<EncenderAplicacion amigos={solidFriends} userWebID={webID}/>
+			<FriendNotification amigos={solidFriends}/>
+		</div>
 	);
 	
 }
