d9f4f6fd73468f051406dfeda559a9d1917558f2
Rcenador <rocio.cenador@solidlines.io>
2021-04-21 22:16:30 +0200

    test FormRango.js

--- webapp/src/tests/FormRango.test.js
+++ webapp/src/tests/FormRango.test.js
@@ -1,14 +1,35 @@
 import React from "react";
+import { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { configure } from "enzyme";
 import { render } from "@testing-library/react";
 import FormRango from "../components/localizacion/FormRango";
 
   
   const { container } = render(<FormRango />);
-  
+  configure({ adapter: new Adapter() });
+
   describe("FormRango", () => {
     it("renders without crashing", () => {
       expect(container).toBeTruthy();
     });
   });
 
+  test("should render form rango", () => {
+ 
+    const args = {
+      amigos: [],
+      userWebID: "webIdExample",
+      lat: 43,
+      lon: 5
+    };
   
+    const wrapper = shallow(
+      <FormRango amigos={args.amigos} userWebID={args.userWebID} lat={args.lat} lon={args.lon}/>
+    );
+    const mockEvent = { target: { value: "This is just for test" } };
+    wrapper.find("FormControl").simulate("change", mockEvent);
+
+    const { getByText } = render(<FormRango/>);
+    expect(getByText("Area de visualización:")).toBeInTheDocument();
+  });
