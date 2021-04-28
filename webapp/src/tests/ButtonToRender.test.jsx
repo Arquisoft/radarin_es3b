2c453ae4f5d297aeb8ae60ad8c7f5ac1f867fdd5
Rcenador <rocio.cenador@solidlines.io>
2021-04-28 21:50:55 +0200

    Update ButtonToRender.test.jsx

--- webapp/src/tests/ButtonToRender.test.jsx
+++ webapp/src/tests/ButtonToRender.test.jsx
@@ -1,12 +1,45 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import ButtonToRender from "../components/ButtonToRender";
+import { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { configure } from "enzyme";
 
-  
-  const { container } = render(<ButtonToRender />);
-  
-  describe("ButtonToRender", () => {
+const { container } = render(<ButtonToRender />);
+configure({ adapter: new Adapter() });
+
+describe("ButtonToRender", () => {
     it("renders without crashing", () => {
-      expect(container).toBeTruthy();
+        expect(container).toBeTruthy();
     });
-  });
+});
+
+
+
+test("ButtonToRender works", async () => {
+
+    
+
+    const { getByText, getAllByRole } = render(<ButtonToRender />);
+
+
+    const processOn = getAllByRole("button", { color: "primary" }).find(element => element.className.includes("MuiButton"));
+    expect(processOn).toBeInTheDocument();
+
+    const processOff = getAllByRole("button", { color: "secondary" }).find(element => element.className.includes("MuiButton"));
+    expect(processOff).toBeInTheDocument();
+    
+    fireEvent.click(processOn);
+    fireEvent.click(processOff);
+
+    expect(container).toBeTruthy();
+
+    const wrapper = shallow(
+      <ButtonToRender/>
+    );
+
+    wrapper.setState({showComponent: true});
+    
+});
