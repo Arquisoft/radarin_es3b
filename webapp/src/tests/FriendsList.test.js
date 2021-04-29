1d8d239b6f58c952d5a942c3d869e2c8cc08f141
Rcenador <rocio.cenador@solidlines.io>
2021-04-29 12:06:10 +0200

    test for new friendList functionality

--- webapp/src/tests/FriendsList.test.js
+++ webapp/src/tests/FriendsList.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import FriendsList from "../components/solid/FriendsList";
+import { fireEvent } from "@testing-library/react";
 
   
   const { container } = render(<FriendsList />);
@@ -10,3 +11,26 @@ import FriendsList from "../components/solid/FriendsList";
       expect(container).toBeTruthy();
     });
   });
+
+
+  test("user can type webID url and click Add or Delete friend", async () => {
+
+    const { getAllByRole, getByPlaceholderText } = render(<FriendsList />);
+
+    const friendURL = getByPlaceholderText("WebID/URL");
+    expect(friendURL).toBeInTheDocument();
+
+    
+    fireEvent.change(friendURL, {target: { value: "ejemploURL" }});
+
+    const addFriend = getAllByRole("button", { color: "primary" }).find(element => element.className.includes("MuiButton"));
+    expect(addFriend).toBeInTheDocument();
+
+    const deleteFriend = getAllByRole("button", { color: "secondary" }).find(element => element.className.includes("MuiButton"));
+    expect(deleteFriend).toBeInTheDocument();
+    
+    fireEvent.click(addFriend);
+    fireEvent.click(deleteFriend);
+
+    expect(deleteFriend).toBeInTheDocument();
+});
